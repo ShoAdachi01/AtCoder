9d1c17d6d294fb9afcd9bd863ef97b4a379472ad
Shoichiro Adachi <169110662+ShoAdachi01@users.noreply.github.com>
2024-05-09 18:51:13 -0400

    A - Divisible

---
+++ ABC347/A - Divisible.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+    vector<int> A(N);
+    for (int i = 0; i < N; i++){
+        cin >> A[i];
+    }
+    for (int i = 0; i < N; i++) {
+        if(A[i] % K == 0){
+            cout << A[i] / K << " ";
+        }
+    }
+
+return 0;
+}
