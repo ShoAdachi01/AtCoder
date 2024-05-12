631e0629530df7ff3ee00d734ca7c214de647239
Shoichiro Adachi <169110662+ShoAdachi01@users.noreply.github.com>
2024-05-11 22:56:58 -0400

    AtCoder ABC342

---
+++ ABC342/A - Yay!.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    string S;
+    cin >> S;
+    int cnt = 0;
+    bool first = true;
+    for (int i = 1; i < S.length(); i++ ){
+        if(S[i] == S[0]) {
+            first = false;
+            break;
+        }
+    }
+    for (char c : S) {
+        if (c == S.front()) {
+            cnt++;
+        }else {
+            cnt++;
+            break;
+        } 
+
+    }
+    if(first) {
+        cout << 1;
+    } else {
+    cout << cnt; 
+    }    
+
+return 0;
+}
