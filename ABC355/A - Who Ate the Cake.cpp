093f836996043542292486bca74e99547e2129c7
Shoichiro Adachi <169110662+ShoAdachi01@users.noreply.github.com>
2024-05-25 12:27:57 -0400

    AtCoder Beginner Contest

---
+++ ABC355/A - Who Ate the Cake.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#define rep(i,N) for (int i = 0; i < N; ++i)
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+
+    if (A == B) {
+        cout << "-1";
+    }else {
+        cout << 6 - (A + B);
+    }
+
+return 0;
+}
