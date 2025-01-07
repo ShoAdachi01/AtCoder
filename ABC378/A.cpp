0ef4d697f0337ba7727564674a9abb504780ec8d
Sho <shoadachi1101@gmail.com>
2025-01-06 22:23:05 -0500

    ABC378 A Question Added

--- ABC378/A.cpp
+++ ABC378/A.cpp
@@ -1,11 +1,19 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std; 
-
 int main(){
-    int a1, a2, a3, a4;
-    cin >> a1 >> a2 >> a3 >> a4;
-    find(a1 )
+    int cnt = 0;
+    vector<int> a(4);
+    for(int i = 0; i < 4; i++) {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    for(int i = 1; i <= 3; i++) {
+        if(a[i - 1] == a[i]) {
+            cnt++;
+            i++;
+        }
 
+    }
+    cout << cnt;
     return 0;
 }
