3c1813979dd577969b5bcbe3dda972da875b41f0
ShoAdachi <shoadachi1101@gmail.com>
2025-05-20 22:20:13 +0900

    One just before sleeping. Doing LeetCode and AtCoder Problems Daily

---
+++ ABC404/A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string S;
+    cin >> S;
+    vector<char> letters(S.begin(), S.end());
+    while(true) { 
+        for(int i = 97; i <= 122; ++i) {
+            char c = (char)i;
+            if (find(letters.begin(), letters.end(), c) == letters.end()) {
+                cout << c << endl;
+                return 0;
+            }
+        }
+    }
+    
+
+}s
